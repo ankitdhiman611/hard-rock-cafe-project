1a982415d4d8df70d29104479b6f33e7d1f1f548
Ankit Dhiman <ankitdhiman@192.168.1.17>
2024-10-06 11:06:59 +0530

    make a dynamic cafe web site using html,css and js exploring the different food items and offers

---
+++ index.js
@@ -0,0 +1,40 @@
+
+
+function review() {
+    document.getElementById("sameer").style.display = 'block';
+}
+function close_div() {
+    document.getElementById("sameer").style.display = 'none';
+}
+
+
+
+function login() {
+    document.getElementById("ankit").style.display = 'block';
+}
+
+function login_again() {
+    document.getElementById("keshav").style.display = 'none';
+    document.getElementById("ankit").style.display = 'block';
+}
+
+function close_login() {
+    document.getElementById("ankit").style.display = 'none';
+}
+
+
+function signup() {
+    document.getElementById("keshav").style.display = 'block';
+}
+function signup_again() {
+    document.getElementById("keshav").style.display = 'block';
+    document.getElementById("ankit").style.display = 'none';
+
+}
+
+function close_signup() {
+    document.getElementById("keshav").style.display = 'none';
+}
+
+
+
